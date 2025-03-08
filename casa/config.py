b9ca1292483fb0b666dc649e45f480da417c68c6
vwoolvett <vwoolvett@gmail.com>
2025-03-08 13:16:40 -0300

    Modified .gitignore and deleted casa/config.py for next commit

--- casa/config.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-measurespath = "~/data"  # WARN: keep this in sync with entrypoint.sh, compose.yml
-
-logfile = f"{time.strftime("%Y_%m_%d_%H",time.localtime())}.log"
-telemetry_enabled = False
-crashreporter_enabled = False
